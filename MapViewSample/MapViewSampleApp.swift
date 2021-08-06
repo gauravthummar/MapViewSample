84318c6956ac9c16ddaf421745e2ae150ddd44cd
Gauravkumar <gauravkumar_thummar@homedepot.com>
2021-08-06 10:53:56 -0400

    Your message about the commit

---
+++ MapViewSample/MapViewSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapViewSampleApp.swift
+//  MapViewSample
+//
+//  Created by Gauravkumar Thummar on 2021-08-06.
+//
+
+import SwiftUI
+
+@main
+struct MapViewSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
